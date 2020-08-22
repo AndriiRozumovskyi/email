984c07d2f008f73896b29f68f8205fce3f448c98
AndriiRozumovskyi <an.zebra@gmail.com>
2020-08-22 23:19:01 +0300

    init commit

---
+++ src/studentdatabaseapp/StudentDatabaseApp.java
@@ -0,0 +1,12 @@
+package studentdatabaseapp;
+
+public class StudentDatabaseApp {
+    public static void main(String[] arg) {
+    Student stu1 = new Student();
+
+    // Ask how many new users we want to add
+
+        // Create n number of new students
+    }
+
+}
